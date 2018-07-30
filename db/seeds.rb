14624a7df49098d8381ac8df379450bd65b31fb7
Travis Cole <iamtraviscole@gmail.com>
2018-07-29 23:34:18 -0400

    generated User scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create([
+  {username: 'user1', email: 'user1@email.com', password: 'user1pw'},
+  {username: 'user2', email: 'user2@email.com', password: 'user2pw'},
+  {username: 'user3', email: 'user3@email.com', password: 'user3pw'},
+])
