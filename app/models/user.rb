b48022d9d4a70c871637637c2fcd790f8017c625
Travis Cole <iamtraviscole@gmail.com>
2018-08-02 21:45:46 -0400

    added user auth via JWT using knock gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :monsters, dependent: :destroy
   has_many :unlocked_bodies
   has_many :bodies, through: :unlocked_bodies
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :users
     resources :monsters
+    post 'user_token' => 'user_token#create'
   end
 
 end
