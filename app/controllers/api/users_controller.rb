cf0d3ad3b5706df326163a721053071962231c3c
Travis Cole <iamtraviscole@gmail.com>
2018-08-06 00:38:01 -0400

    changed default id route param to username for Users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,16 +1,17 @@
 class Api::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
+  before_action :authenticate_user
 
   # GET /users
   def index
     @users = User.all
 
-    render json: @users
+    render json: @users, except: [:email, :password_digest]
   end
 
   # GET /users/1
   def show
-    render json: @user
+    render json: @user, except: [:email, :password_digest]
   end
 
   # POST /users
@@ -41,7 +42,7 @@ class Api::UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.find_by(username: params[:username])
     end
 
     # Only allow a trusted parameter "white list" through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    resources :users
+    resources :users, param: :username
     resources :monsters
     post 'user_token', to: 'user_token#create'
     get 'current_user_info', to: 'current_user#current_user_info'
