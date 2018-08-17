6f31b7f4c6b381593333495e9172445e49080cd6
Travis Cole <iamtraviscole@gmail.com>
2018-08-16 21:26:37 -0400

    added basic user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :monsters, dependent: :destroy
   has_many :unlocked_bodies
   has_many :bodies, through: :unlocked_bodies
+  validates :username, :email, :password, presence: true
+  validates :username, :email, uniqueness: {case_sensitive: false}
+  validates :email, format: /@/
+  validates :password, confirmation: {case_sensitive: true}
 end
