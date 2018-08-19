93c134102c9481a20477f8dfb553bcf66321e92f
Travis Cole <iamtraviscole@gmail.com>
2018-08-19 17:30:28 -0400

    added validation for spaces in username / email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :bodies, through: :unlocked_bodies
   validates :username, :email, :password, presence: true
   validates :username, :email, uniqueness: {case_sensitive: false}
+  validates :username, :email, format: { without: /\s/, message: "must contain no spaces" }
   validates :email, format: /@/
   validates :password, confirmation: {case_sensitive: true}
 end
