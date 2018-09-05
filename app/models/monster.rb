f2aa8481e9e175a5fb4b66abb4f1a1f76816c24c
Travis Cole <iamtraviscole@gmail.com>
2018-09-04 22:45:56 -0400

    added formatted date method to monster model

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -2,4 +2,8 @@ class Monster < ApplicationRecord
   belongs_to :user
   validates :body_type, presence: true
   validates_length_of :name, maximum: 25, message: 'too long'
+
+  def created_at_day_year
+    created_at.strftime('%b %e, %Y')
+  end
 end
