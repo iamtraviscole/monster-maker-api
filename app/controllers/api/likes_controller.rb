6b4e426655bf1b1f3498c205655d1f5669b1ca10
Travis Cole <iamtraviscole@gmail.com>
2018-09-15 00:53:33 -0400

    showing monster as response to like and unlike

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -2,17 +2,26 @@ class Api::LikesController < ApplicationController
   before_action :authenticate_user
 
   def like
-    @user = current_user
-    puts 'CURRENT USER', @user
-    @monster = Monster.find(params[:monster_id])
-    @user.likes.create(monster_id: @monster.id)
+    user = current_user
+    monster = Monster.find(params[:monster_id])
+    user_like = user.likes.build(monster_id: monster.id)
+
+    if user_like.save
+      monster_with_associations = Monster.monster_with_associations(monster)
+      render json: monster_with_associations
+    end
   end
 
   def unlike
-    @user = current_user
-    @like = @user.likes.find_by(monster_id: params[:monster_id])
-    @monster = Monster.find(params[:monster_id])
-    @like.destroy
+    user = current_user
+    puts 'CURRENT USER', user.username
+    like = user.likes.find_by(monster_id: params[:monster_id])
+
+    if like.destroy
+      monster = Monster.find(params[:monster_id])
+      monster_with_associations = Monster.monster_with_associations(monster)
+      render json: monster_with_associations
+    end
   end
 
 end
